4f711f7f428ca8abdf54e5b4d0119f2429512d10
Salvatore <salvodanna@yahoo.it>
2023-06-17 00:45:24 +0200

    Esercizio 12 svolto

---
+++ src/EsercizioWhileLoop.java
@@ -0,0 +1,17 @@
+public class EsercizioWhileLoop {
+    public static void main(String[] args) {
+        int daysToSell = 0;
+        int poundsToSell = 10000;
+        int poundsPerDay = 1000;
+
+        while (poundsToSell > 0) {
+            double poundExchange = 1 + (Math.random() * 0.2);
+            daysToSell++;
+            if (poundExchange > 1.15) {
+                poundsToSell -= poundsPerDay;
+                System.out.println("Day " + daysToSell + " Remainings days to sell : " + poundsToSell);
+            }
+        }
+        System.out.println("It took " + daysToSell + " to sell all the pounds");
+    }
+}
